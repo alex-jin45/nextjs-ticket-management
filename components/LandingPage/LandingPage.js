4ace3e484f4f630519d102ebd88545f7f7b8c9b6
alex-jin45 <zhengzhengjin999@gmail.com>
2023-01-07 12:01:39 -0700

    adding faux log in button

--- components/LandingPage/LandingPage.js
+++ components/LandingPage/LandingPage.js
@@ -1,6 +1,15 @@
+import { use, useState } from "react";
+
 import Link from "next/link";
 
 const LandingPage = ({}) => {
+  const [logged, setLogged] = useState(false);
+
+  const logHandler = () => {
+    setLogged(true);
+    console.log("done");
+  };
+
   return (
     <>
       <div className="bg-red-500">
@@ -8,7 +17,8 @@ const LandingPage = ({}) => {
           <ul className="flex justify-between">
             <li>TBD NAME</li>
             <li className="mr-10">
-              <Link href="/dashboard">Dashboard</Link>
+              {!logged ? <button onClick={logHandler}>Log In</button> : null}
+              {logged ? <Link href="/dashboard">Dashboard</Link> : null}
             </li>
           </ul>
         </nav>
