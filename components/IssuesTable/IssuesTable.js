fa79293d383d31b4f61ae1b8ecb22fa8adf56dc0
alex-jin45 <zhengzhengjin999@gmail.com>
2023-01-09 11:03:32 -0700

    table component

--- components/IssuesTable/IssuesTable.js
+++ components/IssuesTable/IssuesTable.js
@@ -1,35 +1,49 @@
+import { FaReact } from "react-icons/fa";
+
 const IssuesTable = ({}) => {
   return (
     <>
-      <div>
-        <table>
+      <div className=" border-4">
+        <table className="w-full">
           <thead>
-            <tr>
-              <th>Issue</th>
-              <th>Level</th>
-              <th>Events</th>
-              <th>Users</th>
+            <tr className="border-b-2">
+              <th className="p-3 text-left">Issue</th>
+              <th className="p-3 text-left">Level</th>
+              <th className="p-3 text-left">Events</th>
+              <th className="p-3 text-left">Users</th>
             </tr>
           </thead>
-
-          <tbody>
-            <tr>
-              <td>
+          <tbody className="">
+            {/* Export into seperate component */}
+            <tr className="border-b-1">
+              <td className="flex  items-center p-4">
+                <FaReact className="mr-4 text-2xl " />
                 <div>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  </p>
+                  <div>
+                    <span>TypeError :&nbsp;</span>
+                    Cannot read properties of undefined (reading length)
+                  </div>
+                  <div>
+                    at eval (webpack-internal:///./pages/index.tsx:37:7)
+                  </div>
                 </div>
               </td>
 
-              <td>
-                <div>
-                  <p>32</p>
+              <td className=" p-4">
+                <div className="w-fit flex p-2 items-center h-5 rounded-md">
+                  <span>Error</span>
                 </div>
               </td>
+              <td className=" p-4">
+                <span>105</span>
+              </td>
 
-              {/*  */}
+              <td className=" p-4">
+                <span>105</span>
+              </td>
             </tr>
+
+            {/*  */}
           </tbody>
         </table>
       </div>
