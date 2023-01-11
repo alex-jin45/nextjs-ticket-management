772b948a4c19c56e2d856f6eefe39f5b4dd6ca05
alex-jin45 <zhengzhengjin999@gmail.com>
2023-01-11 11:35:02 -0700

    extracting data out

--- components/ActivityPage/ActivityGraph.js
+++ components/ActivityPage/ActivityGraph.js
@@ -3,20 +3,26 @@ import { ArcElement, Chart as ChartJs, Legend, Tooltip } from "chart.js/auto";
 import { Line } from "react-chartjs-2";
 
 const ActivityGraph = ({}) => {
+  const labels = ["", "", "", "", "", "Users", "", "", "", "", ""];
+
+  const data = [0, 60, 75, 32, 55, 22, 12, 90, 64, 77, 10];
+
+  const datasets = [
+    {
+      id: 1,
+      label: "Issues",
+      data,
+    },
+  ];
+
   return (
     <>
       <div className="flex">
         <Line
           datasetIdKey="id"
           data={{
-            labels: ["", "", "", "", "", "Users", "", "", "", "", ""],
-            datasets: [
-              {
-                id: 1,
-                label: "Issues",
-                data: [0, 60, 75, 32, 55, 22, 12, 90, 64, 77, 10],
-              },
-            ],
+            labels,
+            datasets,
           }}
         />
       </div>
