ecb1e90912e680cf9139d61c713df2fc161d5f3a
alex-jin45 <zhengzhengjin999@gmail.com>
2023-01-11 11:31:50 -0700

    adding more data

--- components/ActivityPage/ActivityGraph.js
+++ components/ActivityPage/ActivityGraph.js
@@ -2,21 +2,19 @@ import { ArcElement, Chart as ChartJs, Legend, Tooltip } from "chart.js/auto";
 
 import { Line } from "react-chartjs-2";
 
-ChartJs.register(ArcElement, Tooltip, Legend);
-
 const ActivityGraph = ({}) => {
   return (
     <>
-      <div className="flex ">
+      <div className="flex">
         <Line
           datasetIdKey="id"
           data={{
-            labels: ["", "", "", "Users", "", "", "", "", ""],
+            labels: ["", "", "", "", "", "Users", "", "", "", "", ""],
             datasets: [
               {
                 id: 1,
                 label: "Issues",
-                data: [0, 60, 75, 32, 55, 22, 12, 90, 64],
+                data: [0, 60, 75, 32, 55, 22, 12, 90, 64, 77, 10],
               },
             ],
           }}
