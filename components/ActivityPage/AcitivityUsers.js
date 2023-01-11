3a8beb90c8f6bcc1cbe2a5bb743aeb09de5b321b
alex-jin45 <zhengzhengjin999@gmail.com>
2023-01-11 12:01:11 -0700

    adding table to activity page

--- components/ActivityPage/AcitivityUsers.js
+++ components/ActivityPage/AcitivityUsers.js
@@ -1,26 +1,22 @@
 const ActivityUsers = ({}) => {
   return (
     <>
-      <div className="mt-5">
-        <table className="w-3/5">
-          <thead>
-            <tr className="border-b-2">
-              <th className="p-3 text-left">Name</th>
-              <th className="p-3 text-left"># of Issue Reported</th>
-            </tr>
-            <tbody className="">
-              <tr className="">
-                <td className=" p-4">
-                  <span>John Smith</span>
-                </td>
-
-                <td className="flex p-4">
-                  <span>55</span>
-                </td>
+      <div className="">
+        <div className="mt-8 flex w-3/5 ">
+          <table className="w-full">
+            <thead>
+              <tr className="border-b-2 ">
+                <th className="p-3 text-center">Name</th>
+                <th className="p-3 text-center"># of Issue Reported</th>
               </tr>
-            </tbody>
-          </thead>
-        </table>
+
+              <tr className="">
+                <td className="text-center">John Smith</td>
+                <td className="text-center">4</td>
+              </tr>
+            </thead>
+          </table>
+        </div>
       </div>
     </>
   );
--- components/ActivityPage/ActivityGraph.js
+++ components/ActivityPage/ActivityGraph.js
@@ -3,7 +3,7 @@ import { ArcElement, Chart as ChartJs, Legend, Tooltip } from "chart.js/auto";
 import { Line } from "react-chartjs-2";
 
 const ActivityGraph = ({}) => {
-  const labels = ["", "", "", "", "", "Users", "", "", "", "", ""];
+  const labels = ["", "", "", "", "", "USERS", "", "", "", "", ""];
 
   const data = [0, 60, 75, 32, 55, 22, 12, 90, 64, 77, 8];
 
